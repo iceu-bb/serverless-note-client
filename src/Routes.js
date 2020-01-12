19f011a164753fc0f1a8643ab0b136afbd0234ae
iceu-bb <adamlaskowski737@gmail.com>
2020-01-12 12:44:12 +0100

    Add signup view and logic

--- src/Routes.js
+++ src/Routes.js
@@ -5,12 +5,19 @@ import AppliedRoute from './components/AppliedRoute';
 import NotFound from './containers/NotFound';
 import Home from './containers/Home';
 import Login from './containers/Login';
+import Signup from './containers/Signup';
 
 export default function Routes({ appProps }) {
   return (
     <Switch>
       <AppliedRoute path='/' exact component={Home} appProps={appProps} />
       <AppliedRoute path='/login' exact component={Login} appProps={appProps} />
+      <AppliedRoute
+        path='/signup'
+        exact
+        component={Signup}
+        appProps={appProps}
+      />
       <Route component={NotFound} />
     </Switch>
   );
